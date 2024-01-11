e30774c93af7552831c785cf77682badf0213724
Kendall Pfenning <kendallpfenning@Kendalls-MacBook-Pro.local>
2024-01-10 19:10:06 -0800

    updated all thoughts and single thought

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -3,37 +3,29 @@ const { User, Thought } = require('../models');
 const thoughtController = {
     getAllThoughts(req, res) {
         Thought.find({})
-        .populate({
-            path: "reactions",
-            select: "-__v",
+        .then((data) => {
+            res.json(data);
         })
-        .select('-__v')
-        .sort({ _id: -1 })
-        .then(dbThoughtData => res.json(dbThoughtData))
-        .catch(err => {
-             console.log(err);
-             res.status(400).json(err);
-         });
+        .catch((err) => {
+            res.status(500).json(err);
+        });
     },
-    getThoughtById({ params }, res) {
-        Thought.findOne({ _id: params.id })
-        .populate({
-            path: "reactions",
-             select: "-__v",
-         })
-         .select('-__v')
-         .sort({ _id: -1 })
-         .then(dbThoughtData => {
-            if (!dbThoughtData) {
-                return res.status(404).json({ message: 'No thought found with this id' });
-            }
-            res.json(dbThoughtData);
-        })
-        .catch(err => {
-            console.log(err);
-             res.status(400).json(err);
-         });
+
+    getThoughtById(req,res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+            .select('-__v')
+            .then((data) => {
+                if (!data) {
+                    res.status(404).json({ message: "No thought found with this id" });
+                } else {
+                    res.json(data);
+                }
+            })
+            .catch((err) => res.status(500).json(err));
     },
+
+    
+    
     createThought({ body }, res) {
         Thought.create(body)
             .then(({ _id }) => {
